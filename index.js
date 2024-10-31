58eb49735417c611fa2bfd1fba133e36f82efd2f
whereasjovially <whereasjovially@outlook.com>
2024-10-31 19:22:28 +0400

    added: sending preconf to bolt on holesky

--- index.js
+++ index.js
@@ -4,12 +4,13 @@
 // import list from './commands/list'
 
 import { Command } from 'commander';
-import { createPreconfPayload, getWallet, sendPreconfirmation } from './lib/index.js';
+import { getProposer, getWallet, sendPreconfirmation, getLatestSlot, sleep } from './lib/index.js';
 
 const program = new Command();
 
 async function main() {
   program
+    .command('send')
     .description("My Node CLI")
     .requiredOption("-n, --network <type>", "devnet or holesky")
     .requiredOption("-p, --pvk <type>", "Private key to sign a transaction")
@@ -26,6 +27,7 @@ async function run(options) {
     return;
   }
   const wallet = await getWallet(network, pvk);
+  
   // const nonce = 0;
   let chainId = 0;
   if(network === "devnet"){
@@ -33,10 +35,26 @@ async function run(options) {
   }else{
     chainId = 17000;
   }
-  const nonce = await wallet.getNonce();
-  const { payload, txHash, rpc } = await createPreconfPayload(wallet, nonce, chainId);
-  payload['rpc'] = rpc;
-  sendPreconfirmation(payload)
+  let nonce = await wallet.getNonce();
+  // nonce = 3
+  console.log('nonce', nonce);
+  // let slot = await getLatestSlot(chainId);
+  let proposer = await getProposer(chainId);
+  let count = 1;
+  while(proposer === null){
+    proposer = await getProposer(chainId);
+    count ++;
+    console.log(`tried ${count} times.`);
+
+    await sleep(12)
+  }
+  console.log('proposer', proposer)
+  // const proposer = {
+  //   slot: slot +3,
+  //   sidecar_url:"http://135.181.191.125:8017/",
+  //   type:"bolt"
+  // }
+  const data  = await sendPreconfirmation(wallet, nonce, chainId, proposer);
 }
 
 main()
